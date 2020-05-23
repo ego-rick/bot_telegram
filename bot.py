2a66b661a4b3875b6083c3b2c438a5b8e64b3326
kek4jk <64089144+kek4jk@users.noreply.github.com>
2020-05-23 23:52:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import telebot
 
 bot = telebot.TeleBot("1242336019:AAHOF5MZ3Xib-WNslNhIkE9iIqFA2vaUsRg")
-
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
 bot.polling()
