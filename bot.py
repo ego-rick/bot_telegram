d475f981e062aa9c3f20a00aeba23bfc58511140
kek4jk <64089144+kek4jk@users.noreply.github.com>
2020-05-23 23:51:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import telebot
 
-bot = telebot.TeleBot(" ")
+bot = telebot.TeleBot("1191098101:AAF1gBSye1U2Tbx6-rUQPiWUPuCf15lZpnI")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
